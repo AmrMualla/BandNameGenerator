06e243180064ded640abc5585a3e3066b24b4c3b
AmrMualla <muallaamr@gmail.com>
2021-03-13 21:39:26 +0000

    Added \n.

--- main.py
+++ main.py
@@ -2,10 +2,10 @@
 print("Welcome to Amr's Band Name Generator!")
 
 #2. Ask the user for the city that they grew up in.
-user_city = input("What city did you grow up in?")
+user_city = input("What city did you grow up in?\n")
 
 #3. Ask the user for the name of a pet.
-user_pet = input("What is your pet's name?")
+user_pet = input("What is your pet's name?\n")
 
 #4. Combine the name of their city and pet and show them their band name.
 band_name = user_city + " " + user_pet
